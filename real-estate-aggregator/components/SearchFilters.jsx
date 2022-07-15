296c76b6d9e5dbba60a70fce0689c8936aab5847
Asynclyp <ldzlyp770456668@gmail.com>
2022-07-15 17:12:02 -0500

    Search page done

---
+++ real-estate-aggregator/components/SearchFilters.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import {Flex, Box, Select, Text, Input, Spinner, Icon, Button} from '@chakra-ui/react';
+import {userRouter} from 'next/router';
+import {MdCancel} from 'react-icons/md';
+import Image from 'next/image';
+
+const SearchFilters = () =>{
+    return(
+        <div>
+            SearchFilters
+        </div>
+    )
+}
+    
+
+export default SearchFilters;

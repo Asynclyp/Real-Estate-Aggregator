52acfc83edbbac7171dad4a20b351439e88f5eef
Asynclyp <ldzlyp770456668@gmail.com>
2022-07-14 23:55:55 -0500

    hide key

--- real-estate-aggregator/utils/fetchApi.js
+++ real-estate-aggregator/utils/fetchApi.js
@@ -7,7 +7,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com"
 export const fetchApi = async (url) => {
     const { data } = await axios.get((url),{
         headers: {
-            'X-RapidAPI-Key': 'dc864da060msh255642ab597e753p10214cjsnfc0a72efcc66',
+            'X-RapidAPI-Key': '',
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
         }
     });
